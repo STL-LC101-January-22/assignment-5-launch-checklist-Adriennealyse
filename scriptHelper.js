34483fb8df012f0bd7cc310c1d547f9d5808728b
Adrienne Davis <adrienne@adriennealyse.com>
2022-03-16 21:11:27 -0500

    Updated validateInput function in scriptHelper.js file.

--- scriptHelper.js
+++ scriptHelper.js
@@ -16,13 +16,52 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
-function validateInput(testInput) {
+//. validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate.
+
+/*function validateInput(testInput) {
+   window.addEventListener("load", function(){
+
+   })
+}*/
+
+function validateInput("formField") {
+
+       window.addEventListener("load", function() {
+        
+        let form = document.querySelector("form");
+        let inputResponse = ""
+        form.addEventListener("submit", function(event) {
+            let pilotName = document.querySelector("input[name=pilotName]");
+            let copilotName = document.querySelector("input[name=copilotName]");
+            let fuelLevel = document.querySelector("input[name=fuelLevel]");
+            let cargoMass = document.querySelector("input[name=cargoMass]");
+            if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+                alert("All fields are required!");
+                event.preventDefault();
+
+                return "Empty"
+
+            } else if (typeof(pilotName.value) !== String || copilotName.value !== String) {
+                alert("Is a Number");
+                event.preventDefault();
+
+                return "Is a Number"
+
+            } else if (isNan(fuelLevel.value) == true || cargoMass.value.isNaN) {
+                alert("Not a Number");
+                event.preventDefault();
+                
+                return "Not a Number"
+            }
+        });
+    });
+}
+
+function formSubmission(document.getElementById("testForm"), document.getElementById("faultyItems"), 'pilotName', 'copilotName', 'fuelLevel', 'cargoLevel') {
    
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
+
 
 async function myFetch() {
     let planetsReturned;

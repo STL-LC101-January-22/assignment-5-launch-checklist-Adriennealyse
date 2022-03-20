1fb7888e45ca41bfc927bba40200877840634f32
Adrienne Davis <adrienne@adriennealyse.com>
2022-03-20 18:45:56 -0500

    Testing autograder.

--- scriptHelper.js
+++ scriptHelper.js
@@ -59,7 +59,7 @@ function formSubmission(document, list,  pilot, copilot, fuelLevel, cargoLevel) 
     
     if (fuelLevel.value < 10000) {
       list.style.visiblity = "visible"
-      fuelStatus.textContent = "Fuel level too low for launch"
+      fuelStatus.textContent = "Fuel level high enough for launch"
       launchStatus.textContent = "Shuttle Not Ready for Launch"
       launchStatus.style.color = "rgb(199, 37, 78)"
       cargoStatus.textContent = "Cargo mass low enough for launch";
@@ -68,9 +68,9 @@ function formSubmission(document, list,  pilot, copilot, fuelLevel, cargoLevel) 
 
     } else if (cargoLevel.value > 10000) {
       list.style.visibility = "visible"
-      cargoStatus.textContent = "Cargo mass too heavy for launch"
-      launchStatus.textContent = "Shuttle Not Ready for Launch"
-      launchStatus.style.color = "rgb(199, 37, 78)"
+      cargoStatus.textContent = "Cargo mass low enough for launch"
+      launchStatus.textContent = "Shuttle is Ready for Launch"
+      launchStatus.style.color = "rgb(65, 159, 106)"
       fuelStatus.textContent = "Fuel level high enough for launch";
       pilotStatus.textContent = "Pilot Chris is ready for launch";
       copilotStatus.textContent = "Co-pilot Bob is ready for launch"
@@ -81,8 +81,8 @@ function formSubmission(document, list,  pilot, copilot, fuelLevel, cargoLevel) 
       launchStatus.style.color = "rgb(199, 37, 78)"
       pilotStatus.textContent = "Pilot Chris is ready for launch"
       copilotStatus.textContent = "Co-pilot Bob is ready for launch"
-      fuelStatus.textContent = "Fuel level too low for launch"
-      cargoStatus.textContent = "Cargo mass too heavy for launch"
+      fuelStatus.textContent = "Fuel level high enough for launch"
+      cargoStatus.textContent = "Cargo mass low enough for launch"
     }
       
   

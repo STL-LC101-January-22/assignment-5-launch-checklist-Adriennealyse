b8b9916fe96dcb2ab602c3e7dd8272e7522ea388
Adrienne Davis <adrienne@adriennealyse.com>
2022-03-20 02:00:19 -0500

    Debbuged validateInput function in scriptHelper.js.

--- scriptHelper.js
+++ scriptHelper.js
@@ -21,7 +21,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 
 function validateInput(testInput) {
   //window.addEventListener("load", function() {
-    let form = document.querySelector("form");
+    let form = document.getElementById("testForm");
     form.addEventListener("submit", function(event) {
        let pilotName = document.querySelector("input[name=pilotName]");
        let copilotName = document.querySelector("input[name=copilotName]");

81e815c4b362c1e92db8b035fd0e131a4609117f
Adrienne Davis <adrienne@adriennealyse.com>
2022-03-21 00:52:58 -0500

    Debugging

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,5 @@
 // Write your helper functions here!
-// require('isomorphic-fetch');
+import 'isomorphic-fetch';
 
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
@@ -167,8 +167,15 @@ function pickPlanet(planets) {
   return planets[planetSelector]
 }
 
-module.exports.addDestinationInfo = addDestinationInfo;
-module.exports.validateInput = validateInput;
-module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
-module.exports.myFetch = myFetch;
+
+const _addDestinationInfo = addDestinationInfo;
+export { _addDestinationInfo as addDestinationInfo };
+const _validateInput = validateInput;
+export { _validateInput as validateInput };
+const _formSubmission = formSubmission;
+export { _formSubmission as formSubmission };
+const _pickPlanet = pickPlanet;
+export { _pickPlanet as pickPlanet }; 
+const _myFetch = myFetch;
+export { _myFetch as myFetch };
+

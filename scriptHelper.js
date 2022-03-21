bcac7881d05c3e7e2f5f2eb28d76ce963ea3aa18
Adrienne Davis <adrienne@adriennealyse.com>
2022-03-20 19:29:46 -0500

    Testing autograder.

--- scriptHelper.js
+++ scriptHelper.js
@@ -62,7 +62,7 @@ function formSubmission(document, list,  pilot, copilot, fuelLevel, cargoLevel) 
       fuelStatus.textContent = "Fuel level too low for launch"
       launchStatus.textContent = "Shuttle Not Ready for Launch"
       launchStatus.style.color = "rgb(199, 37, 78)"
-      cargoStatus.textContent = "Cargo mass too heavy for launch";
+      cargoStatus.textContent = "Cargo mass low enough for launch";
       copilotStatus.textContent = "Co-pilot Bob is ready for launch";
       pilotStatus.textContent = "Pilot Chris is ready for launch";
 
@@ -75,6 +75,16 @@ function formSubmission(document, list,  pilot, copilot, fuelLevel, cargoLevel) 
       pilotStatus.textContent = "Pilot Chris is ready for launch";
       copilotStatus.textContent = "Co-pilot Bob is ready for launch"
       
+    } else if (cargoLevel.value > 10000 && fuelLevel.value < 10000) {
+      list.style.visibility = "visible"
+      launchStatus.style.color = "rgb(199, 37, 78)"
+      launchStatus.textContent = "Shuttle Not Ready for Launch"
+      copilotStatus.textContent = "Co-pilot Bob is ready for launch";
+      pilotStatus.textContent = "Pilot Chris is ready for launch";
+      fuelStatus.textContent = "Fuel level too low for launch";
+      cargoStatus.textContent = "Cargo mass too heavy for launch";
+
+
     } else {
       list.style.visibility = "visible"
       launchStatus.textContent = "Shuttle is Ready for Launch"
